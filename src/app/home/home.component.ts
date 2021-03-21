4ba9d99bc5a29ad3e16a631a002a6aa5033b7b2e
Francesco Scassillo <scassillofrancesco@gmail.com>
2021-03-21 22:16:41 +0100

    btn fix

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -24,18 +24,31 @@ export class HomeComponent implements OnInit {
   ngOnInit(): void { }
 
   public commit(){
-    this.git.commit(this.title, this.description);
+    let res = this.git.commit(this.title, this.description);
+    if(res == null){
+      alert("1 Commit Staged");
+    }
   }
 
   public push(){
-    this.git.push(false);
+    let res = this.git.push(false);
+    if(res == null){
+      alert("Commit/s Pushed");
+    }
   }
 
   public onFolderSelected(event){
-    var path = event.target.files[0].path;
+    var path: string = event.target.files[0].path;
+    var x = "";
+    if(path.substr(path.length - 1) == '/'){
+      x = path;
+    } else {
+      var split = path.split("/");
+      x = split.slice(0, split.length - 2).join("/") + "/";
+    }
 
-    this.path = path;
-    GitCommands.GIT_PROJECT_PATH = path;
+    this.path = x;
+    GitCommands.GIT_PROJECT_PATH = x;
     console.log(GitCommands.GIT_PROJECT_PATH); 
   }
 
